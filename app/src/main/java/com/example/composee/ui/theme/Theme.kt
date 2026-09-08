9cac0c51e9cb209c546dadf33175d19364aa494e
kifard007 <161320917+kifard007@users.noreply.github.com>
2026-09-08 18:04:05 +0700

    add slider

--- app/src/main/java/com/example/composee/ui/theme/Theme.kt
+++ app/src/main/java/com/example/composee/ui/theme/Theme.kt
@@ -4,12 +4,15 @@ import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
 
 private val DarkColorScheme = darkColorScheme(
     primary = Purple80,
@@ -55,4 +58,12 @@ fun ComposeeTheme(
         typography = Typography,
         content = content
     )
+    @Composable
+    fun DemoText(message: String, fontSize: Float) {
+        Text(
+            text = message,
+            fontSize = fontSize.sp,
+            fontWeight = FontWeight.Bold
+        )
+    }
 }
