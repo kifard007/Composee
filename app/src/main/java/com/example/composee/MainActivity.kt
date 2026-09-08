44b7bf27bd3c79dc9a65ce02cf1e480a551a3877
kifard007 <161320917+kifard007@users.noreply.github.com>
2026-09-08 17:56:25 +0700

    Delete template

--- app/src/main/java/com/example/composee/MainActivity.kt
+++ app/src/main/java/com/example/composee/MainActivity.kt
@@ -20,28 +20,10 @@ class MainActivity : ComponentActivity() {
         setContent {
             ComposeeTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Compose",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    ComposeeTheme {
-        Greeting("Android")
-    }
-}
